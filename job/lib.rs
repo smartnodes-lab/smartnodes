cffad78dd25e2099587f1c74fed25db8f5617d04
mattjhawken <mattjhawken@gmail.com>
2023-06-01 08:14:37 -0400

    Implementing Job and TaskNet contracts.

--- job/lib.rs
+++ job/lib.rs
@@ -2,19 +2,56 @@
 
 #[ink::contract]
 mod job {
+
+    use ink::prelude::string::String;
+    use ink::primitives::Hash;
+    // use ink::prelude::vec::Vec;
+
     #[ink(storage)]
     pub struct Job {
+        owner: Address,
         title: String,
         description: String,
         reward: u32,
-        reward_distribution: bool, // false: final distribution to best algo, true: equal + gradual
-                                   //   distributions for lower loss
-        n_responses: u32,
-        filters: String
+        reward_dist: bool,
+        // n_responses: u32,
+        // filters: str
+    }
+
+    #[ink(event)]
+    pub struct SentLoss {
+
     }
 
     impl Job {
         #[ink(constructor)]
-        pub fn new() -> Self { Self {} }
+        pub fn new(
+            _title: String,
+            _description: String,
+            _reward: u32,
+            _reward_dist: bool,
+        ) -> Self {
+            let caller = Self::env().caller();
+
+            Self {
+                owner: caller,
+                title: _title,
+                description: _description,
+                reward: _reward,
+                reward_dist: _reward_dist,
+            }
+        }
+
+        #[ink(message)]
+        pub fn send_loss(&mut self, loss: Hash) {
+            let from = self.env().caller();
+
+
+            self.env().emit_event(SentLoss {
+                from
+            });
+        }
+
+        pub fn
     }
 }
--- lib.rs
+++ lib.rs
@@ -3,71 +3,33 @@
 #[ink::contract]
 mod nexus {
 
-    use src::Job;
-    use ink::storage::Mapping;
+    // use src::Job;
+    // use ink::storage::Mapping;
 
     #[ink(storage)]
     pub struct Nexus {
         job_index: i64,
-        index_to_proposal: Mapping<u32, Job>,
+        // index_to_proposal: Mapping<u32, Job>,
         // user_to_amount_funded: Mapping<AccountId, u32>,
         // min_stake: u8
     }
 
     impl Nexus {
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
+        pub fn new() -> Self {
             Self {
                 job_index: 0,
             }
-
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn add_job(&mut self) {
+            self.job_index += 1;
         }
 
-        /// Simply returns the current value of our `bool`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
-    }
-
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let nexus = Nexus::default();
-            assert_eq!(nexus.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut nexus = Nexus::new(false);
-            assert_eq!(nexus.get(), false);
-            nexus.flip();
-            assert_eq!(nexus.get(), true);
+        pub fn get_job_num(self) -> i64 {
+            self.job_index
         }
     }
 }
