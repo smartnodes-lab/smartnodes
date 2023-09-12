8c3a79522d1fa88b1d745c0f533968f87156cbb2
mattjhawken <mattjhawken@gmail.com>
2023-09-12 10:38:03 -0400

    Brining together contracts to form extensive user networks

--- src/contracts/task-net/lib.rs
+++ src/contracts/task-net/lib.rs
@@ -1,7 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
-pub use self::tasknet::TaskNetRef;
-
 #[ink::contract]
 mod tasknet {
     use ink::prelude::vec::Vec;
--- src/contracts/user-net/lib.rs
+++ src/contracts/user-net/lib.rs
@@ -1,11 +1,14 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
+pub use self::usernet::UserNet;
+
 #[ink::contract]
 mod usernet {
 
     use ink::{
         prelude::{
-            vec::Vec
+            vec::Vec,
+            string::String
         },
         storage::Mapping
     };
@@ -16,6 +19,13 @@ mod usernet {
 
     const MAX_FILTER_WORDS: u8 = 5;
 
+    #[derive(Clone, Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum UserNetError {
+        UserExists,
+        FundingTooLow
+    }
+
     /// Holds key user connectivity info,
     #[derive(scale::Decode, scale::Encode, Debug, Clone)]
     #[cfg_attr(
@@ -23,7 +33,7 @@ mod usernet {
         derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
     )]
     pub struct Connection {
-        author: i64,
+        host: i64,
         funding: Balance,
         description: String,
         uids: Vec<i64>,
@@ -45,17 +55,17 @@ mod usernet {
         next_id: ConnectionId
     }
 
-    /// Holds key individual data (pub key, rep, keywords)
+    /// Holds key individual data (f key, rep, keywords)
     #[derive(scale::Decode, scale::Encode, Debug, Clone)]
     #[cfg_attr(
         feature = "std",
         derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
     )]
     pub struct User {
-        public_key: [u8; 392],
+        address: AccountId,
         locked: Balance,
         reputation: i8,
-        filters: [String; 6],
+        filters: [[u8; 15]; 6],
     }
 
     #[derive(Default, scale::Decode, scale::Encode)]
@@ -79,5 +89,37 @@ mod usernet {
         users: Mapping<UserId, User>,
         user_list: Users,
         connections: Connections
+    }
+
+    impl UserNet {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self {
+                users: Mapping::new(),
+                user_list: Users::default(),
+                connections: Connections::default()
+            }
+        }
+
+        #[ink(message, payable)]
+        pub fn create_user(
+            &mut self,
+            user: User
+        ) -> Result<(), UserNetError> {
+            let caller: AccountId = self::env().caller();
+
+            if self.user_list.users.iter().all(|user_address| user_address != caller) {
+                let user_id = self.user_list.next_id;
+                self.user_list.next_id =
+                    user_id.checked_add(1).expect("UserIds Exhausted!");
+                self.users.insert(user_id, user);
+                self.user_list.users.push(user_id);
+                self.env().emit_event()
+            }
+
+            Ok(())
+        }
+
+        fn ensure_caller_is_user(&self, user_id: UserId) {}
     }
 }
